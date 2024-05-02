12f6b836831a54e68fc90527475a9fe98f774e02
JonasBuchadas <jonasmcbuchadas@gmail.com>
2024-05-02 14:15:19 +0100

    .

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -7,7 +7,7 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 	wp core download --allow-root
 	wp config create --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_USER_PASS} --dbhost=${MARIADB_HOST} --allow-root --path=/var/www/html
 	wp core install --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PASS} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
-	wp user create "${WORDPRESS_USER}" "${WORDPRESS_EMAIL}" --role=author --user_pass=${WORDPRESS_PASS} --display_name="${MARIADB_USER}" --allow-root #--path=${WORDPRESS_PATH}
+	wp user create "${WORDPRESS_USER}" "${WORDPRESS_EMAIL}" --role=author --user_pass=${WORDPRESS_PASS} --display_name="${MARIADB_USER}" --path=${WORDPRESS_PATH} --allow-root
 	wp theme install bravada --activate --allow-root
 else
 	echo "Wordpress already installed"
